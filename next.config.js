d1d90c1efd3223cfe3d545248ba890461e7523ba
imber <a1157911285@163.com>
2023-04-26 22:42:59 +0800

    feat: nspage

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-  // i18n: {
-  //   locales: ['cn', 'en'],
-  //   defaultLocale: 'cn'
-  // }
+  reactStrictMode: true, //可以不要
+  i18n: {
+    locales: ['cn', 'en'],
+    defaultLocale: 'cn'
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/account123/**'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
---
+++ pages/demos/async.tsx
@@ -0,0 +1,14 @@
+// nspage
+import { GetServerSideProps } from 'next'
+
+const async = () => {
+  return <div>Enter</div>
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {}
+  }
+}
+
+export default async
